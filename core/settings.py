b947fee944421d08856332ec9dc0481d4ebabd16
Erick Javier Cruz <erickcroz2013@gmail.com>
2024-01-15 18:32:22 -0600

    Created custom user manager

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +83,7 @@ DATABASES = {
       'PORT': env('POSTGRESQL_PORT'),
     }
 }
-
+AUTH_USER_MODEL="users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
